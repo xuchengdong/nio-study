180de6d2c32e6cba324e06e4cf0809f260dc0e8c
xuchengdong <xuchengdongxcd@126.com>
2017-09-19 23:57:17 +0800

    SocketChannel和ServerSocketChannel 读写测试

--- src/main/java/com/df/ClientStart.java
+++ src/main/java/com/df/ClientStart.java
@@ -12,19 +12,51 @@ public class ClientStart {
 
     public ClientStart(String host, int port) throws IOException {
         SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.configureBlocking(false); // Non-blocking Mode
         socketChannel.connect(new InetSocketAddress(host, port));
+
+        while (!socketChannel.finishConnect()) {
+            //wait, or do something else...
+        }
 
         ByteBuffer buf = ByteBuffer.allocate(48);
 
+        // 从服务端读取信息
         int bytesRead = socketChannel.read(buf);
         while (bytesRead != -1) {
             buf.flip();
+
+            StringBuilder builder = new StringBuilder();
+
             while (buf.hasRemaining()) {
-                System.out.print((char) buf.get());
+                builder.append((char) buf.get());
+//                System.out.print((char) buf.get());
+            }
+
+            if (builder.length() > 0) {
+                System.out.println("Receive server messages: " + builder.toString());
             }
 
             buf.clear();
             bytesRead = socketChannel.read(buf);
+            System.out.println("client bytesRead:" + bytesRead);
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // 向服务端写信息
+        String greeting = "Hello Server, I'am " + socketChannel.getLocalAddress();
+        buf.clear();
+        buf.put(greeting.getBytes());
+
+        buf.flip();
+
+        while (buf.hasRemaining()) {
+            socketChannel.write(buf);
         }
 
         socketChannel.close();
--- src/main/java/com/df/ServerStart.java
+++ src/main/java/com/df/ServerStart.java
@@ -16,18 +16,50 @@ public class ServerStart {
 
         serverSocketChannel.socket().bind(new InetSocketAddress(port));
 
+        serverSocketChannel.configureBlocking(false); // Non-blocking Mode
+
         while (true) {
             SocketChannel socketChannel = serverSocketChannel.accept();
 
-            String greeting = "Hello," + socketChannel.getRemoteAddress();
+            if (socketChannel == null) {
+                continue;
+            }
+
             ByteBuffer buf = ByteBuffer.allocate(48);
+
+            // 向客户端写信息
+            String greeting = "Hello," + socketChannel.getRemoteAddress();
             buf.clear();
             buf.put(greeting.getBytes());
 
             buf.flip();
 
-            while(buf.hasRemaining()) {
+            while (buf.hasRemaining()) {
                 socketChannel.write(buf);
+            }
+
+            socketChannel.shutdownOutput(); // 结束向客户端写操作,之后不能再写,否则抛异常,执行此语句后客户端read(buf)才能返回-1否则返回的是0
+
+            // 从客户端读取信息
+            buf.clear();
+            int bytesRead = socketChannel.read(buf);
+
+            System.out.println("server bytesRead:" + bytesRead);
+
+            while (bytesRead != -1) {
+                buf.flip();
+
+                StringBuilder builder = new StringBuilder();
+
+                while (buf.hasRemaining()) {
+                    builder.append((char) buf.get());
+//                    System.out.print((char) buf.get());
+                }
+
+                System.out.println("Receive client messages: " + builder.toString());
+
+                buf.clear();
+                bytesRead = socketChannel.read(buf);
             }
 
             socketChannel.close();
